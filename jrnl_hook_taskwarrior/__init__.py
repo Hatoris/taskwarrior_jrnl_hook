149dbf68bf147d74d919e7c0593e7be901d58e49
Florian Bernard <florianxbernard@gmail.com>
2018-09-30 00:51:08 -0400

    final version

--- jrnl_hook_taskwarrior/__init__.py
+++ jrnl_hook_taskwarrior/__init__.py
@@ -11,28 +11,29 @@ from taskw import TaskWarrior
 
 TIME_FORMAT = '%Y%m%dT%H%M%SZ'
 
-# read config info from taskwarrior config file
-w = TaskWarrior()
-TASK_CONFIG = w.load_config()
-
-# read infos from taskrc config file
-JRNL_NAME = TASK_CONFIG.get('jrnl_name', 'default')
-JRNL_CONFIG_PATH = os.path.expanduser(TASK_CONFIG.get('jrnl_config', '~/.jrnl_config'))
-ADD_TAGS = TASK_CONFIG.get('add_tags', True)
-ADD_PROJECT = TASK_CONFIG.get('add_project', False)
-JRNL_BY_MONTH = TASK_CONFIG.get('jrnl_by_month', False)
-LANGUAGE = TASK_CONFIG.get('language', 'en')
-FILTER_TAGS = TASK_CONFIG.get('filter_tags', False)
-
-# read info from jrnl_config
-with open(JRNL_CONFIG_PATH, "r") as f:
-    JRNL_CONFIG = json.load(f)
-TAGS_SYMBOL = JRNL_CONFIG.get("tagsymbols", "@")
-
 original = json.loads(sys.stdin.readline())
 modified = json.loads(sys.stdin.readline())
 
 if 'start' in modified and 'start' not in original:
+
+    # read config info from taskwarrior config file
+    w = TaskWarrior()
+    TASK_CONFIG = w.load_config()
+
+    # read infos from taskrc config file
+    JRNL_NAME = TASK_CONFIG.get('jrnl_name', 'default')
+    JRNL_CONFIG_PATH = os.path.expanduser(TASK_CONFIG.get('jrnl_config', '~/.jrnl_config'))
+    ADD_TAGS = TASK_CONFIG.get('add_tags', True)
+    ADD_PROJECT = TASK_CONFIG.get('add_project', False)
+    JRNL_BY_MONTH = TASK_CONFIG.get('jrnl_by_month', False)
+    LANGUAGE = TASK_CONFIG.get('language', 'en')
+    FILTER_TAGS = TASK_CONFIG.get('filter_tags', False)
+
+    # read info from jrnl_config
+    with open(JRNL_CONFIG_PATH, "r") as f:
+        JRNL_CONFIG = json.load(f)
+    TAGS_SYMBOL = JRNL_CONFIG.get("tagsymbols", "@")
+
     title = modified["description"]
 
     #add taskwarrior task to title
